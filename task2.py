02cbf1c8330153d1509a3d41f36fdaad7ac678c9
ChorniyR <pomak803@gmail.com>
2021-07-01 13:03:58 +0300

    fix task2 and task6

--- task2.py
+++ task2.py
@@ -1,7 +1,7 @@
 def is_polindrome(number):
     if list(str(number)) == list(str(number))[::-1]:
         return True
-    else:
-        return False
+    return False
+
 
 print(is_polindrome(1221))

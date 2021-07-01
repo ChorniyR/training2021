8e2be7ade77321bd69c34a1596a3ee5c95393489
roman <pomak803@gmail.com>
2021-07-01 10:34:37 +0300

    task7 was completed

---
+++ task7.py
@@ -0,0 +1,15 @@
+def find_prefix(strs):
+    prefix = strs[0][:1]
+    i = 1
+    while True:
+        for str in strs:
+            if len(prefix) > 1 and prefix != str[:i]:
+                return prefix[:i-1]
+            if prefix != str[:i]:
+                return 0        
+        i += 1
+        prefix = str[:i]
+        
+print(find_prefix(["floower","floow","flwight"]))
+
+    

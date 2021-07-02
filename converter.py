13fe61eecd887b0d9afc4fff7f699c69abf74437
ChorniyR <pomak803@gmail.com>
2021-07-02 15:34:14 +0300

    upgrade read method, also it can't work with brabrackets

--- converter.py
+++ converter.py
@@ -56,9 +56,7 @@ class SCVHandler(Converter):
         self._path = path
         self._lines = None
         self._titles = None
-
-    def get_as_dict(self):
-        pass
+        self._data = self.read()
 
     def convert(self):
         pass
@@ -73,20 +71,39 @@ class SCVHandler(Converter):
         for index, line in enumerate(self._lines):
             if index > 0:
                 object_ = {}
-                for i in range(0, len(line.split(","))):
-                    try:
-                        object_.update({self._titles[i]: line.split(",")[i]})
-                    except IndexError:
-                        print("Index out of range")
-                        pass
-                data.append(object_)
+                coll_patterns = self._find_collect_patterns(line)
+                if not coll_patterns:
+                    for i in range(0, len(line.split(","))):
+                        try:
+                            object_.update({self._titles[i]: line.split(",")[i]})
+                        except IndexError:
+                            print("Index out of range")
+                    data.append(object_)
+                else:
+                    i = 0
+                    for part in line.split('"'):
+                        if part not in coll_patterns:
+                            for p in part.split(","):
+                                if p != "":
+                                    try:
+                                        object_.update({self._titles[i]: part.split(",")[i]})
+                                        i += 1
+                                    except IndexError:
+                                        pass
+                        else:
+                            try:
+                                object_.update({self._titles[i]: part})
+                                i += 1
+                            except IndexError:
+                                pass
+                    data.append(object_)
         return data
 
     def parse_titles(self):
         return tuple(self._lines[0].split(","))
 
     @staticmethod
-    def _find_coll_patterns(line):
+    def _find_collect_patterns(line):
         separator = "qwe"
         new_line = line.replace('""', '"').replace('""', separator)
         try:
@@ -97,10 +114,11 @@ class SCVHandler(Converter):
             str_patterns = re.findall(r'qwe(.*)qwe', new_lines)
             return str_patterns
 
+    def __iter__(self):
+        return iter(self._data)
+
 
 if __name__ == '__main__':
-    scv_handler = SCVHandler(".\data\data_file.scv")
-    for obj in scv_handler.read():
-        print(obj)
-
-
+    scv_handler = SCVHandler(r".\data\data_file.scv")
+    for i in scv_handler:
+        print(i)
